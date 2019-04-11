49476f780ee761da76e205678569de9eb74b0a7c
Roman_Pidgurskiy <romaliviv@gmail.com>
2019-04-12 00:03:49 +0300

    added first project to github

---
+++ Lesson_5_3/src/ua/lviv/lgs/Application.java
@@ -0,0 +1,11 @@
+package ua.lviv.lgs;
+
+public class Application {
+   public static void main(String[] args) {
+
+	   System.out.println("Double.MAX_VALUE "+Double.MAX_VALUE);
+	   System.out.println("Double.MIN_VALUE "+Double.MIN_VALUE);
+	   System.out.println("Float.MAX_VALUE "+Float.MAX_VALUE);
+	   System.out.println("Float.MIN_VALUE "+Float.MIN_VALUE);
+}
+}
